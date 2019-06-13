c51c2e8ee9f4d67530635a0546d009d347395f07
Dawid Śremski <dawid.sremski@asseco.pl>
2019-06-13 08:50:35 +0200

    Create login and sign up pages.

--- frontend/inspectorbudget/src/App.js
+++ frontend/inspectorbudget/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Layout, notification} from "antd";
+import AppHeader from "./AppHeader";
+import Login from "./Login/Login";
+import {Route, Switch, withRouter} from "react-router-dom";
+import {getCurrentUser} from "./APIUtils";
+import SignUp from "./SignUp/SignUp";
+const { Content } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: null,
+            isAuthenticated: false,
+            isLoading: false
+        };
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+        this.loadCurrentUser = this.loadCurrentUser.bind(this);
+    }
+
+    handleLogin() {
+        notification.success({
+            message: "You're successfully logged in.",
+            description: `Welcome in Inspector Budget, ${this.state.currentUser.userName}`
+        });
+        this.loadCurrentUser();
+        this.props.history.push("/");
+    }
+
+    handleLogout() {
+
+    }
+
+    loadCurrentUser() {
+        this.setState({
+            isLoading: true
+        });
+        getCurrentUser().then(response => {
+            this.setState({
+                currentUser: response,
+                isAuthenticated: true,
+                isLoading: false
+            });
+        }).catch(error => {
+            this.setState({
+                isLoading: false
+            });
+        });
+    }
+
+    render() {
+        return (
+            <Layout className="app-container">
+                <AppHeader
+                    isAuthenticated={this.state.isAuthenticated}
+                    currentUser={this.state.currentUser}
+                    onLogout={this.handleLogout()}
+                />
+                <Content className="app-content">
+                        <Switch>
+                            <Route path="/login" render={(props) =>
+                                <Login onLogin={this.handleLogin} {...props} />}
+                            />
+                        </Switch>
+                        <Switch>
+                            <Route path="/signup" render={(props) =>
+                                <SignUp/>
+                            }/>
+                        </Switch>
+                </Content>
+            </Layout>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
